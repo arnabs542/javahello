ea1e546a99883d2d67ea37bb67578b7d223d01e9
linglingithub <linglin.mail@gmail.com>
2017-12-15 00:01:12 -0800

    'u'

--- src/hw/ReOrderLinkedList.java
+++ src/hw/ReOrderLinkedList.java
@@ -19,8 +19,6 @@ L = 1 -> 2 -> 3 -> null, is reordred to 1 -> 3 -> 2 -> null
  *
  */
 
-
-
 /**
  * class ListNode {
  *   public int value;
@@ -33,70 +31,72 @@ L = 1 -> 2 -> 3 -> null, is reordred to 1 -> 3 -> 2 -> null
  */
 
 import utils.ListNode;
+
 public class ReOrderLinkedList {
-  public ListNode reorder(ListNode head) {
-    if (head == null)
-      return null;
-    // find the two halves of thel list;
-    ListNode mid = this.middleNode(head);
-    ListNode one = head, two = mid.next;
-    mid.next = null;
-    
-    // reverse the last half;
-    two = this.reverse(two);
-    
-    // merge the two list;
-    return this.merge(one, two);
-  }
-  
-  
-  private ListNode middleNode(ListNode head) {
-	    // write your solution here
-	    if(head == null)
-	      return null;
-	    ListNode dummy = new ListNode(-1);
-	    dummy.next = head;
-	    ListNode fast = dummy, slow = dummy;
-	    while(fast != null && fast.next != null){  //!!! first check fast
-	      slow = slow.next;
-	      fast = fast.next.next;
-	    }
-	    return slow;
-	  }
-	  
+	public ListNode reorder(ListNode head) {
+		if (head == null)
+			return null;
+		// find the two halves of thel list;
+		ListNode mid = this.middleNode(head);
+		ListNode one = head, two = mid.next;
+		mid.next = null;
+
+		// reverse the last half;
+		two = this.reverse(two);
+
+		// merge the two list;
+		return this.merge(one, two);
+	}
+
+	private ListNode middleNode(ListNode head) {
+		// write your solution here
+		if (head == null)
+			return null;
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+		ListNode fast = dummy, slow = dummy;
+		while (fast != null && fast.next != null) { // !!! first check fast
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
 	private ListNode reverse(ListNode head) {
-    // write your solution here
-    if (head == null)
-      return null;
-    ListNode dummy = new ListNode(-1);
-    dummy.next = head;
-    ListNode curr = head, tail = head;
-    ListNode cnext = curr.next;
-    while(cnext != null){
-      ListNode nnext = cnext.next;
-      cnext.next = dummy.next;
-      dummy.next = cnext;
-      cnext = nnext;
-    }
-    tail.next = null;
-    return dummy.next;
-  }
-  
-  private ListNode merge(ListNode one, ListNode two){
-    ListNode dummy = new ListNode(-1);
-    ListNode tail = dummy;
-    while (one != null && two != null){
-      tail.next = one;
-      tail = tail.next;
-      tail.next = two;
-      tail = tail.next;
-      one = one.next;
-      two = two.next;
-    }
-    if (one != null){
-      tail.next = one;
-    }
-    return dummy.next;
-  }
-  
+		// write your solution here
+		if (head == null)
+			return null;
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+		ListNode curr = head, tail = head;
+		ListNode cnext = curr.next;
+		while (cnext != null) {
+			ListNode nnext = cnext.next;
+			cnext.next = dummy.next;
+			dummy.next = cnext;
+			cnext = nnext;
+		}
+		tail.next = null;
+		return dummy.next;
+	}
+
+	private ListNode merge(ListNode one, ListNode two) {
+		ListNode dummy = new ListNode(-1);
+		ListNode tail = dummy;
+		while (one != null && two != null) {
+			tail.next = one;
+			one = one.next;
+			tail = tail.next;
+			tail.next = two;
+			tail = tail.next;
+			// one = one.next; //should update right after!! before tail change the next to
+			// two
+			two = two.next;
+		}
+		if (one != null) {
+			tail.next = one;
+		}
+		return dummy.next;
+	}
+
 }
