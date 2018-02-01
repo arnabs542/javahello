95910bd2bded56ccf2ba3c49887f2724fa25ad41
linglingithub <linglin.mail@gmail.com>
2018-01-31 22:05:35 -0800

    update

---
+++ src/hw/Random1000UsingRandom5.java
@@ -0,0 +1,18 @@
+package hw;
+
+public interface Random1000UsingRandom5 {
+	public int random1000() {
+		while (true) {
+			// The following 4 lines of code is usually used to compute 
+			// a0*x^0 + a1*x^1 + a2*x^2 + a3*x^3 + ... + ak*x^k
+			int num = 0;
+			for (int i = 0; i < 5; i++) {
+				num = num * 5 + RandomFive.random5();
+			}
+			// choose 3000 instead of 1000 to reduce the # of expected random5() calls.
+			if (num < 3000) {
+				return num % 1000;
+			}
+		}
+	}
+}
