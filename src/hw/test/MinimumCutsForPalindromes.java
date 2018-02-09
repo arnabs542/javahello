dfab5afd6c0313b987452819d353fc55b1f4381b
linglingithub <linglin.mail@gmail.com>
2018-02-09 00:09:23 -0800

    u

--- src/hw/test/MinimumCutsForPalindromes.java
+++ src/hw/test/MinimumCutsForPalindromes.java
@@ -12,7 +12,7 @@ package hw.test;
  * 
  * Assumptions
  * 
- * The given string is not null 
+ * The given string is not null
  * 
  * Examples
  * 
@@ -27,6 +27,36 @@ package hw.test;
  */
 public class MinimumCutsForPalindromes {
 	public int minCuts(String input) {
+		if (input == null || input.length() <= 1) {
+			return 0;
+		}
+		char[] chars = input.toCharArray();
+		int n = chars.length;
+		boolean[][] isPal = new boolean[n+1][n+1];
+		// substring[i-1, j-1] is palindrome or not.
+		int[] minCut = new int[n + 1];
+		minCut[0] = -1; // important!! since when for the whole string is palindrome, use 0 cut, --> con't
+		// but method use 1+minCut[end-1], so need to set [0] as -1 to get the number out.
+		// minCut[i]: string of length i, substring[0, i-1]
+		for (int i = 2; i <= n; i++) {
+			minCut[i] = i - 1;
+			for (int j = i; j >= 1; j--) { // not >= 0, should be >= 1
+				// because the index meaning of i, j should be the same, and --> isPal's index --> j's stop range
+				if (chars[j-1] == chars[i-1]) {
+					if (j + 1 >= i - 1 || isPal[j + 1][i - 1] == true) {
+						isPal[j][i] = true;
+						minCut[i] = Math.min(minCut[i], 1 + minCut[j-1]); // since [j-1, i-1] is palindrome, here should be j-1, not minCut[j]
+						// since j - 1 can be 0, which means whole string is palindrome, either put extra check here, or put minCut[0] = -1 before
+					} else {
+						isPal[j][i] = false;
+					}
+				}
+			}
+		}
+		return minCut[n];
+	}
+
+	public int minCuts_Ref(String input) {
 		// Assumptions: input is not null.
 		char[] array = input.toCharArray();
 		int len = array.length;
@@ -44,7 +74,7 @@ public class MinimumCutsForPalindromes {
 			for (int start = end; start >= 1; start--) {
 				// calculate isP[start][end] first.
 				if (array[start - 1] == array[end - 1]) {
-					isP[start][end] = end - start < 2 || isP[start+1][end - 1];
+					isP[start][end] = end - start < 2 || isP[start + 1][end - 1];
 				}
 				// use isP[start][end] to calculate minCuts[end].
 				if (isP[start][end]) {
@@ -53,6 +83,13 @@ public class MinimumCutsForPalindromes {
 			}
 		}
 		return minCuts[len] - 1;
-
+	}
+	
+	public static void main(String[] args) {
+		MinimumCutsForPalindromes sol = new MinimumCutsForPalindromes();
+		String input = "ababbbabbababa"; // expect 3
+		String input1 = "aaaaaabbabb"; // expect 1
+		int result = sol.minCuts(input);
+		System.out.println(result);
 	}
 }
