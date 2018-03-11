abac366dd619f2c06c6c5d475bff1ea41129a2b7
linglingithub <linglin.mail@gmail.com>
2018-03-10 18:35:48 -0800

    update

--- src/hw/LongestCommonSubsequence.java
+++ src/hw/LongestCommonSubsequence.java
@@ -20,6 +20,29 @@ package hw;
  *
  */
 public class LongestCommonSubsequence {
+	public int longest_ref(String s, String t) {
+		// Assumptions: s, t are not null
+		// using the trick of "add 1" index will make the base case
+		// easier to handle, e.g. the 0th row and the 0th column will
+		// be all 0s.
+
+		int m = s.length();
+		int n = t.length();
+		// dp[i][j]: the lcs of substring s[0,i-1] and t[0,j-1]
+		int[][] longest = new int[m + 1][n + 1];
+		for (int i = 1; i < m + 1; i++) {
+			for (int j = 1; j < n + 1; j++) {
+				if (s.charAt(i - 1) == t.charAt(j - 1)) {
+					longest[i][j] = longest[i - 1][j - 1] + 1;
+				} else {
+					longest[i][j] = Math.max(longest[i - 1][j], longest[i][j - 1]);
+				}
+			}
+		}
+		return longest[m][n];
+
+	}
+	
 	public int longest(String s, String t) {
 		// Assumptions: s, t are not null
 		if (s.length() == 0 || t.length() == 0) {
@@ -36,6 +59,9 @@ public class LongestCommonSubsequence {
 				} else {
 					dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
 					dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+					// ref use only [i-1][j], [i][j-1]
+					// --> right, actually in this problem [i-1][j-1] are already 
+					// covered by [i-1][j] / [i][j-1]
 				}
 			}
 		}
--- src/hw/LongestCommonSubstring.java
+++ src/hw/LongestCommonSubstring.java
@@ -23,9 +23,40 @@ import java.util.List;
  *
  */
 public class LongestCommonSubstring {
-
-	// self, ac
+	
 	public String longestCommon(String s, String t) {
+		// Assumptions: s, t are not null
+		char[] sa = s.toCharArray();
+		char[] ta = t.toCharArray();
+		
+		// record the longest common substring's start position in s.
+		int start = 0;
+		// record the longest common substring's length.
+		int longest = 0;
+		int m = s.length();
+		int n = t.length();
+		int[][] common = new int[m][n];
+		
+		for (int i = 0; i < sa.length; i++) {
+			for (int j = 0; j < ta.length; j++) {
+				if (sa[i] == ta[j]) {
+					if (i == 0 || j == 0) {
+						common[i][j] = 1;
+					} else {
+						common[i][j] = common[i - 1][j - 1] + 1;
+					}
+					if (common[i][j] > longest) {
+						longest = common[i][j];
+						start = i - longest + 1;
+					}
+				} 
+			}
+		}
+		return s.substring(start, start + longest);
+	}
+
+	// self, ac, use hashmap for shorter string
+	public String longestCommon_self_ac(String s, String t) {
 		// Assumptions: s, t are not null.
 		if (s.length() == 0 || t.length() == 0) {
 			return new String();
