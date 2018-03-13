902ad0c5c875162147b7bd95ae93da80a1e9ffa5
linglingithub <linglin.mail@gmail.com>
2018-03-13 08:30:40 -0700

    up

---
+++ src/hw/MaximumValuesOfSizeKSlidingWindows.java
@@ -0,0 +1,34 @@
+package hw;
+
+/**
+ * 
+ * Maximum Values Of Size K Sliding Windows
+ * 
+ * Description
+ * 
+ * Given an integer array A and a sliding window of size K, find the maximum
+ * value of each window as it slides from left to right.
+ * 
+ * Assumptions
+ * 
+ * The given array is not null and is not empty
+ * 
+ * K >= 1, K <= A.length
+ * 
+ * Examples
+ * 
+ * A = {1, 2, 3, 2, 4, 2, 1}, K = 3, the windows are {{1,2,3}, {2,3,2}, {3,2,4},
+ * {2,4,2}, {4,2,1}},
+ * 
+ * and the maximum values of each K-sized sliding window are [3, 3, 4, 4, 4]
+ * 
+ * Medium Deque
+ * 
+ * @author
+ *
+ */
+public class MaximumValuesOfSizeKSlidingWindows {
+	public List<Integer> maxWindows(int[] array, int k) {
+		// Write your solution here
+	}
+}
