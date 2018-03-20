49b94b4a7bd7c0d3f708e38f5dfac5c019e64b67
Ling Lin <ling.lin@afficienta.com>
2018-03-19 22:06:21 -0700

    u

---
+++ src/hw/GetCountArray.java
@@ -0,0 +1,19 @@
+package hw;
+/**
+ * 
+ * 
+ * 
+ * 
+ * @author 
+ *
+ */
+public class GetCountArray {
+	public int[] countArray(int[] array) {
+		// The indexArray contains the indices in the original array,
+		// and it will be sorted by the corresponding number in the 
+		// original array.
+		// The countArray is the actual return array.
+		// The helper array is to help the merge sort.
+		
+	}
+}
