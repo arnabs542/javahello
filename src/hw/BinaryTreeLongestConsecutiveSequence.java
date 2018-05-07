506cd6d597f440b01ee811a1acd3021b0cf3b102
linglingithub <linglin.mail@gmail.com>
2018-05-07 00:36:46 -0700

    u

--- src/hw/BinaryTreeLongestConsecutiveSequence.java
+++ src/hw/BinaryTreeLongestConsecutiveSequence.java
@@ -16,13 +16,85 @@ import utils.TreeNode;
 
 
 public class BinaryTreeLongestConsecutiveSequence {
+	public int longestConsecutive_REF1(TreeNode root) {
+		// REF, bottom up recursion
+		if (root == null) {
+			return 0;
+		}
+		int[] max = new int[1];
+		dfs(root, max);
+		return max[0];
+	}
+	
+	/**
+	 *  the return value represent the longest consecutive sequence from the current
+	 *  root (with current root included)
+	 */
+	private int dfs(TreeNode root, int[] max) {
+		// base case
+		if (root == null) {
+			return 0;
+		}
+		
+		int cur = 1;
+		int left = dfs(root.left, max);
+		int right = dfs(root.right, max);
+		
+		if (root.left != null && root.key + 1 == root.left.key) {
+			cur = Math.max(cur, 1 + left);
+		}
+		if (root.right != null && root.key + 1 == root.right.key) {
+			cur = Math.max(cur, 1 + right);
+		}
+		max[0] = Math.max(max[0], cur);
+		return cur;
+	}
+	
+	/**
+	 * Top down way
+	 * @param root
+	 * @return
+	 */
+	public int longestConsecutive_REF2(TreeNode root) {
+		// REF, top down recursion
+		if (root == null) {
+			return 0;
+		}
+		int[] max = new int[1];
+		dfs(root, root.key, 0, max);
+		return max[0];
+	}
+	
+	private void dfs(TreeNode root, int prev, int longest, int[] max) {
+		// base case
+		if (root == null) {
+			return;
+		}
+		// longest = 0.
+		if (prev == root.key - 1) {
+			longest++;
+		} else {
+			longest = 1;
+		}
+		max[0] = Math.max(max[0], longest);
+		dfs(root.left, root.key, longest, max);
+		dfs(root.right, root.key, longest, max);
+	}
+
+	
+	
+	
   /**
   * basic idea: top down, passing the current length uptill current node, 
   * if current nodes's child == cur'val + 1, do path + 1
   * else, starting from 1
   * updating a global max path length during the DFS
+  * 
+  * Time: O(n) -- traversal
+  * Space: O(height)
+  * 
   **/
-  public int longestConsecutive(TreeNode root) {
+  public int longestConsecutive_Self(TreeNode root) {
     // Write your solution here
     if (root == null) {
     	return 0;
