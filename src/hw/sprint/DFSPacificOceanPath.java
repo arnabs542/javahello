420cde2c5b63cd1e4ded864beb7639cf98ef65a1
linglingithub <linglin.mail@gmail.com>
2018-05-08 20:17:20 -0700

    u

--- src/hw/sprint/DFSPacificOceanPath.java
+++ src/hw/sprint/DFSPacificOceanPath.java
@@ -1,6 +1,12 @@
 package hw.sprint;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * 
+ * 417. Pacific Atlantic Water Flow
+ * 
  * 
  * Given an m * n matrix of non-negative integers representing the height of each unit cell in a continent,
  * the "Pacific Ocean" touches the left and top edges of the matrix and the "Atlantic ocean" 
@@ -27,5 +33,44 @@ package hw.sprint;
  *
  */
 public class DFSPacificOceanPath {
-
+	public List<int[]> pacificAtlantic(int[][] matrix) {
+		List<int[]> res = new ArrayList<>();
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return res;
+		}
+		int n = matrix.length, m = matrix[0].length;
+		boolean[][] pacific = new boolean[n][m];
+		boolean[][] atlantic = new boolean[n][m];
+		for (int i = 0; i < n; i++) {
+			dfs(matrix, pacific, Integer.MIN_VALUE, i, 0);
+			dfs(matrix, atlantic, Integer.MIN_VALUE, i, m - 1);
+		}
+		for (int j = 0; j < m; j++) {
+			dfs(matrix, pacific, Integer.MIN_VALUE, 0, j);
+			dfs(matrix, atlantic, Integer.MIN_VALUE, n - 1, j);		
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (pacific[i][j] && atlantic[i][j]) {
+					res.add(new int[] {i, j});
+				}
+			}
+		}
+		return res;
+	}
+	
+	final int[][] DIRS = {
+		{-1, 0}, {1, 0}, {0, 1}, {0, -1}	
+	};
+	
+	private void dfs(int[][] matrix, boolean[][] visited, int height, int x, int y) {
+		int n = matrix.length, m = matrix[0].length;
+		if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y] || matrix[x][y] < height) {
+			return;
+		}
+		visited[x][y] = true;
+		for (int[] dir : DIRS) {
+			dfs(matrix, visited, matrix[x][y], x + dir[0], y + dir[1]);
+		}
+	}
 }
